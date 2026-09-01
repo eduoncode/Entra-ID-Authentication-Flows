0e83a475e89db2a9bf02a3ba61b85b7153217ee3
Eduardo Fernandes <edufernandes.1895@gmail.com>
2026-09-01 14:46:07 -0300

    feat: add @nestjs/config dependency and configure global settings in UserModule fix: update RolesGuard to use getAllAndOverride for role checks fix: change default port from 3000 to 3002 in main.ts

--- apps/api-nestjs/src/auth/guards/roles.guard.ts
+++ apps/api-nestjs/src/auth/guards/roles.guard.ts
@@ -16,12 +16,11 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.get<string[]>(
-      'roles',
-      context.getHandler(),
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
     );
-    if (!requiredRoles) return true;
-
+    if (!requiredRoles || requiredRoles.length === 0) return true;
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
--- apps/api-nestjs/src/user/user.module.ts
+++ apps/api-nestjs/src/user/user.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { AuthModule } from '@auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
   controllers: [UserController],
 })
 export class UserModule {}
